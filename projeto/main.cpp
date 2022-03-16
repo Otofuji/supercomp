a193d2141237c5d1d48efe60a32ed1bfa26379ba
Eric Fernando Otofuji Abrantes <ericfoa@al.insper.edu.br>
2022-03-16 17:47:21 -0300

    checkpoint

--- projeto/main.cpp
+++ projeto/main.cpp
@@ -10,8 +10,8 @@ struct matriz { //Com base em exemplo do professor e em https://people.engr.tamu
     double max;
 };
 
-int smith_waterman(int &n, int &m, int &x, int &y) {
-    //REFEREÊNCIAS
+int smith_waterman(int &n, int &m, int &a, int &b) {
+    //REFERÊNCIAS
     //Martin Mann, Mostafa M Mohamed, Syed M Ali, and Rolf Backofen
     //Interactive implementations of thermodynamics-based RNA structure and RNA-RNA interaction prediction approaches for example-driven teaching
     //PLOS Computational Biology, 14 (8), e1006341, 2018.
@@ -19,47 +19,69 @@ int smith_waterman(int &n, int &m, int &x, int &y) {
     //Freiburg RNA tools: a central online resource for RNA-focused research and teaching
     //Nucleic Acids Research, 46(W1), W25-W29, 2018.
 
-
-    //usar matriz clássica do C++ cm struct
+    //Usar matriz clássica do C++ cm struct
     //Criando uma matriz clássica do C++ usando struct, tendo por base exemplo do professor e https://people.engr.tamu.edu/djimenez/ut/utsa/cs1723/lecture3.html
     matriz alinhamento[n][m];
+
+    string notation;
+    string tempa = ' ';
+    string tempb = ' ';
+    int maximum = 0;
+
+    tempa.append(n, a); //https://www.techiedelight.com/append-char-end-string-cpp/
+    tempb.append(m, b);
+    a = tempa;
+    b = tempb;
     
-    //Inicializar H[i,0]=0, 0≤i≤n
-    //Inicializar H[0,j]=0, 0≤j≤m
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
             alinhamento[i][j].max = 0; //Por conveniência, a matriz inteira já foi inicializada com zeros
         }
     }
-    
+
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++) {
+            if (a[i] == b[j]) {
+                alinhamento[i-1][j-1].max++;
+                alinhamento[i-1][j-1].max++;
+                alinhamento[i-1][j-1].x = i-1;
+                alinhamento[i-1][j-1].y = j-1;
+
+                if (alinhamento[i-1][j-1].max > maximum) {
+                    maximum = alinhamento[i-1][j-1].max;
+                }
+            }
             
+            else {
+                if (a[i] != b[j]) {
+                    alinhamento[i-1][j-1].max--;
+                    if (alinhamento[i-1][j-1].max < 0) {
+                        alinhamento[i-1][j-1].max = 0;
+                    }
+                }
+            }
         }
     }
+
+    cout << '****************************' << endl;
+    cout << a << endl;
+    cout << notation << endl;
+    cout << b << endl;
+    cout << '****************************' << endl;
 
     //Match = +2
     //Mismatch = -1
     //Gap = -1
 
-    //Para cada 1≤i≤n e 1≤j≤m:
-    //          Calcular diagonal = H[i-1,j-1] + w(a[i],b[j]), onde w(a[i],b[j])=2 se houve match,
-    //                       w(a[i],b[j])= -1 se houve mismatch e
-    //                       w(a[i],b[j])= -1 se houve gap
-    //          Calcular deleção  = H[i-1,j] - 1
-    //          Calcular inserção = H[i,j-1] - 1
-    //          Calcular H[i,j]=máximo (0, diagonal, deleção, inserção)
-    //Retornar o máximo de H[_,_]
-    
-    return max;
+    return maximum;
 }
 
 int main() {
     int n; int m;
-    string x; string y;
-    cin >> n >> m >> x >> y;
+    string a; string b;
+    cin >> n >> m >> a >> b;
     
-    int smith_waterman_max = smith_waterman(&n, &m, &x, &y);
+    int smith_waterman_max = smith_waterman(&n, &m, &a, &b);
 
     return 0;
 }
